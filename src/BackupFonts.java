5fc457aa1ca3e21fb50cf118744b1a9bb0375c25
Henry W <htwerner01@gmail.com>
2022-02-14 18:19:00 -0600

    Add CLI progress bar

--- src/BackupFonts.java
+++ src/BackupFonts.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.nio.file.Files;
 
 public class BackupFonts {
-    public static void main(String[] args) throws IOException, FontFormatException {
+    public static void main(String[] args) throws Exception {
         int progress = 0;
         int progressEndPoint = 0;
 
@@ -31,12 +31,15 @@ public class BackupFonts {
             return;
         }
         progressEndPoint = fontList.length * 2;
+        System.out.println("Files found: " + fontList.length);
 
         for (File x : fontList) {
             System.out.print(x.getName() + " ");
             Font f = Font.createFont(Font.TRUETYPE_FONT, x); //What is a fontFormatException and how do I account for it?
-            System.out.println(f.getName());
             progress++;
+            //System.out.println(f.getName());
+            updateProgressBar(f.getName(), progress, progressEndPoint);
+            
 
             File fBackup = new File("./fonts/" + f.getName() + ".otf"); //create output path with proper name and filetype
             if (fBackup.isFile())
@@ -44,8 +47,36 @@ public class BackupFonts {
 
             Files.copy(x.toPath(), fBackup.toPath()); //copy original file to the output path
             progress++;
+            updateProgressBar("Saved.", progress, progressEndPoint);
+        }
+    }
+
+    private static void updateProgressBar(String txt, int progCurrent, int progEnd) throws Exception {
+        char tickChar = '█', emptyChar = '_', startChar = '│', endChar = '│';
+        int barLength = 75;
+        float progressState = (progCurrent / (float)progEnd) * 100; 
+        String printedStateValue = String.format("%02d", (int)progressState);
+        printedStateValue = progCurrent + "/" + progEnd + " " + printedStateValue; // debug math
+        printedStateValue = " " + printedStateValue + '%';
+
+        String progressBar = "" + startChar;
+        for (int x = 0; x < barLength; x++) {
+            if (x <= progressState / 100 * barLength) 
+                progressBar += tickChar;
+            else
+                progressBar += emptyChar;
+        }
+        progressBar += endChar + printedStateValue + "\r";
+
+        while (txt.length() < barLength + 2 + printedStateValue.length()) { // the +6 is the chars for beginning, end, and printedStateVlaue
+            txt += " ";
         }
 
-        //System.out.println("Total progress: " + progress + "/" + progressEndPoint);
+        System.out.print(txt + "\n" + progressBar);
+
+        if (progCurrent == progEnd)
+            System.out.println("\nDone.");
+
+        Thread.sleep(10);
     }
 }
